6638d7fd1195974522e4f7d50d686362fd6f745d
jdiperi88 <jdiperi88@yahoo.com>
2018-07-01 11:44:13 -0400

    connected hardcoded auth id to shared.js action file

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,15 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
-export function handleInitialData () {
+import { setAuthedUser } from '../actions/authedUser'
+
+
+const AUTHED_ID = 'tylermcginnis'
     return (dispatch) => {
         return getInitialData()
             .then(({users, tweets}) =>{
                 dispatch(receiveUsers(users))
                 dispatch(receiveTweets(tweets))
+                dispatch(setAuthedUser(AUTHED_ID))
             })
     }
-}
