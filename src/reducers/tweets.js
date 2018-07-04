9829bd81fcf5babd07f0b97fe73003947e3d4e24
jdiperi88 <jdiperi88@yahoo.com>
2018-07-04 15:16:47 -0400

    added has liked functionality to tweet component

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
 
 export default function tweet (state = {}, actions){
     switch(actions.type){
@@ -7,6 +7,16 @@ export default function tweet (state = {}, actions){
                 ...state,
                 ...actions.tweets
             }
+        case TOGGLE_TWEET:
+        return {
+            ...state,
+            [actions.id]: {
+                ...state[actions.id],
+                likes: actions.hasLiked === true
+                    ? state[actions.id].likes.filter((uid) => uid !== actions.authedUser)
+                    : state[actions.id].likes.concat([actions.authedUser])
+            }
+        }
         default :
             return state
     }
