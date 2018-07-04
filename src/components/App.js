d01ce54cfd402f507dcc7b33eb260dde3c64dafb
jdiperi88 <jdiperi88@yahoo.com>
2018-07-04 17:32:11 -0400

    added replying to functionality and TweetPage Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared'
 import Dasboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
   componentDidMount(){
@@ -15,7 +16,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true 
           ? <h2> loading... </h2>
-          :<NewTweet />}
+          :<TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />}
         
       </div>
     )
