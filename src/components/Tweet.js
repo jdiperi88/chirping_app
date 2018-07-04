317f6c59ed3e03c0e4860c8e8c3b6daebf3dc292
jdiperi88 <jdiperi88@yahoo.com>
2018-07-04 10:13:20 -0400

    added avatar to tweet component and installed react-icon package

---
+++ src/components/Tweet.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatTweet } from '../utils/helpers'
+import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
+import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
+import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
+class Tweet extends Component {
+    render() {
+        console.log(this.props)
+        const {tweet } = this.props
+        if(tweet  === null){
+            return <p> This tweet doesnt exist</p>
+        }
+        const {
+            name,avatar, timestamp,text, hasLiked, likes, replies, id, parent
+        } = tweet
+        return( 
+            <div className='tweet' >
+                <img 
+                    src={avatar}
+                    alt={`Avatar of ${name}`}
+                    className='avatar'
+                    />
+            </div>
+        )
+    }
+}
+
+function mapStateToProps ({authedUser, users, tweets}, { id }) {
+    const tweet = tweets[id];
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
+  
+    return {
+      authedUser,
+      tweet: tweet
+        ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+        : null
+    };
+  }
+
+export default connect(mapStateToProps)(Tweet)
